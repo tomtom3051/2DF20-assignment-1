451a182bb4c29d3d8127fa3406a1f1a5431e0268
AMa-11 <137224267+AMa-11@users.noreply.github.com>
2023-09-24 14:16:27 +0200

    Updated part 1 to be with histogram

--- part1.py
+++ part1.py
@@ -6,33 +6,28 @@ import matplotlib.pyplot as plt
 # change this to large n to see the difference in the plots
 # for large n the distributions are similar
 # i.e -> inf
-n = 100
+n = 10000000
 
-sampleSize = 10000
+sampleSize = n
 
 #Poisson setup
-alpha = random.random()*n
+alpha = random.random()*1000
 alpha = round(alpha, 1)
 print(alpha)
 Poisson = stats.poisson(alpha)
 xs1 = Poisson.rvs(sampleSize)
 xs1 = num.sort(xs1)
-print(xs1)
 
 #Binom Setup
 Binom = stats.binom(n, alpha/n)
 xs2 = Binom.rvs(sampleSize)
 xs2 = num.sort(xs2)
-print(xs2)
 
-
-#don't know the theory of why this works gonna try to explain this later
-n = len(xs1)
-ys = num.arange(1/n, 1+1/n, 1/n)
 
 fig, axs = plt.subplots(1, 2, tight_layout=True)
-axs[0].step(xs1, ys)
-axs[1].step(xs2, ys)
+axs[0].hist(xs1, bins=20)
+axs[1].hist(xs2, bins=20)
+print("mean binom: " +  str(num.mean(xs2)))
+print("mean pois: " + str(num.mean(xs1)))
+plt.title("Comparison between Poisson (left) and Binom (right)")
 plt.show()
-print(num.mean(xs1))
-print(num.mean(xs2))
